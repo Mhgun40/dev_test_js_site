99be37a85d8379f472a9e1eba1154d193cdf3855
Mhgun40 <mhgun40@gmail.com>
2023-03-16 16:36:37 +0100

    modif text

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Bonjour je suis patricia la magnifique la copine de valoche de mimi et de sylvie, vous pouvez me retrouver sur https://chaturbaite/patoch_la_cochone.com]</p>
+        <p>[Bonjour je suis patricia la magnifique la copine de valoche de mimi et de sylvie, vous pouvez me retrouver sur https://chaturbite/patoch_la_cochone.com]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
