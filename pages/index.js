5d48ea7838c18ee6cfdac5e4920a90518751635c
Mhgun40 <mhgun40@gmail.com>
2023-03-16 16:21:52 +0100

    initialisation

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Bonjour je suis patricia la magnifique la copine de valoche de mimi et de sylvie, vous pouvez me retrouver sur https://chaturbaite/patoch_la_cochone.com]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
